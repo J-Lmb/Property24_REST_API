954d478a8b25b815ee45ff2c898a4bd65bf5feca
Joseph Lumbahe <39498415+J-Lmb@users.noreply.github.com>
2020-02-25 01:25:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -45,6 +45,9 @@ app.use('/api/user', authRoute);
 mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true}, () => console.log('connected to DB!'))
 
 //listen to server
-app.listen(3000)
-
-console.log("Listening on port 3000");
+//app.listen(3000)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+//console.log("Listening on port 3000");
